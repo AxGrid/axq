5d34066b3355021c383c850fd4215b3761f214d1
Илья Каран <lifeofhucci@MacBook-Pro-Ila.local>
2024-03-11 18:21:20 +0300

    interfaces from domaim

--- domain/writer.go
+++ domain/writer.go
@@ -4,13 +4,8 @@
 
 package domain
 
-import "github.com/golang/protobuf/proto"
-
 type Writer interface {
 	Push(message []byte) error
-	PushMany(messages [][]byte) error
-	PushProto(message proto.Message) error
-	PushProtoMany(messages []proto.Message) error
 	Close()
 	GetOpts() ServiceOpts
 	Counter() (uint64, error)
